ddc5eaa6292e5b8d6249da658164fbb162b0d06e
harish8 <harishbabu.webmaster@gmail.com>
2016-02-22 13:46:48 -0500

    updated db file added status table

--- DB/yii2DB_advanced.sql
+++ DB/yii2DB_advanced.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 09, 2015 at 05:16 PM
+-- Generation Time: Feb 22, 2016 at 01:43 PM
 -- Server version: 5.5.46-0ubuntu0.14.04.2
 -- PHP Version: 5.5.9-1ubuntu4.14
 
@@ -38,7 +38,31 @@ CREATE TABLE IF NOT EXISTS `migration` (
 
 INSERT INTO `migration` (`version`, `apply_time`) VALUES
 ('m000000_000000_base', 1446761199),
-('m130524_201442_init', 1446761213);
+('m130524_201442_init', 1446761213),
+('m151113_231038_create_status_table', 1447457638);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
+CREATE TABLE IF NOT EXISTS `status` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `message` text COLLATE utf8_unicode_ci NOT NULL,
+  `permissions` smallint(6) NOT NULL DEFAULT '0',
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `status`
+--
+
+INSERT INTO `status` (`id`, `message`, `permissions`, `created_at`, `updated_at`) VALUES
+(1, 'here is my test message 1 in my new yii2 project', 1, 6, 7),
+(2, 'Here is the test status 2 after modifying the form and model class', 10, 1447461929, 1447461929);
 
 -- --------------------------------------------------------
 
@@ -60,14 +84,15 @@ CREATE TABLE IF NOT EXISTS `user` (
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `password_reset_token` (`password_reset_token`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `username`, `auth_key`, `password_hash`, `password_reset_token`, `email`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'harish8', 'a68KsZ1rgqQDJVfVhbmkh0Njjwny6d7x', '$2y$13$.DDqnJTx3lQ/fpu91Mtb0.Ptn/tZ3Vd/e9UD0S97T1KJCPtMBQVLG', NULL, 'harishbabu@harish8.com', 10, 1446761474, 1446761474);
+(1, 'harish8', 'a68KsZ1rgqQDJVfVhbmkh0Njjwny6d7x', '$2y$13$.DDqnJTx3lQ/fpu91Mtb0.Ptn/tZ3Vd/e9UD0S97T1KJCPtMBQVLG', 'HPPCKRU4jy9ROAJIqwn7ywe22yrxRcW__1447288654', 'harishbabu@harish8.com', 10, 1446761474, 1447288654),
+(2, 'demo', '1wjKnuXBTa285K7JpWairiwkhQmPlicW', '$2y$13$LKwOHOAHGr8XQf.vMJoIreE7MnORz/9ALsQqybqqbbj.Gihn3HTeq', NULL, 'demo@example.com', 10, 1450657264, 1450657264);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
